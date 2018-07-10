491a8e520b5dbd30e7eb46204c11616698a2b11e
neemchand <neemchand@ucreate.co.in>
2018-07-10 18:32:00 +0530

    helper and view project details updated

--- resources/views/project-detail-view.blade.php
+++ resources/views/project-detail-view.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading"></div>
+                <div class="panel-heading">{{ getProjectName(5,1)}} </div>
 
                 <div class="panel-body">
                     @if (session('status'))
