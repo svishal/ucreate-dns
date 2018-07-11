7dd7275ec409407cba2e3fa82ebbd66c5c886f30
inderpreet singh <inderpreetsingh@ucreate.co.in>
2018-07-11 15:13:51 +0530

    Pre populating the data in project edit page

--- resources/views/create-edit-project.blade.php
+++ resources/views/create-edit-project.blade.php
@@ -41,39 +41,39 @@
                         <label>URL</label>
                         <input type="text" name="url" value="{{($project->url)??old('url')}}">
                         <label>Domain Registrar</label>
-                        <input type="text" name="domain_registrar" value="{{($project->domain_registrar)??old('domain_registrar')}}">
+                        <input type="text" name="domain_registrar" value="{{($project->projectDetail->domain_registrar)??old('domain_registrar')}}">
                         <label>Registrant</label>
-                        <input type="text" name="registrant" value="{{($project->registrant)??old('registrant')}}">
+                        <input type="text" name="registrant" value="{{($project->projectDetail->registrant)??old('registrant')}}">
                         <label>Contact</label>
-                        <input type="text" name="contact" value="{{($project->contact)??old('contact')}}">
+                        <input type="text" name="contact" value="{{($project->projectDetail->contact)??old('contact')}}">
                         <label>Name Servers</label>
-                        <input type="text" name="name_servers" value="{{($project->name_servers)??old('name_servers')}}">
+                        <input type="text" name="name_servers" value="{{($project->projectDetail->name_servers)??old('name_servers')}}">
                         <label>Website Title</label>
-                        <input type="text" name="website_title" value="{{($project->website_title)??old('website_title')}}">
+                        <input type="text" name="website_title" value="{{($project->projectDetail->website_title)??old('website_title')}}">
                         <label>Website Description</label>
-                        <textarea name="website_description">{{($project->website_description)??old('website_description')}}</textarea>
+                        <textarea name="website_description">{{($project->projectDetail->website_description)??old('website_description')}}</textarea>
                         <label>Language</label>
-                        <input type="text" name="language" value="{{($project->language)??old('language')}}">
+                        <input type="text" name="language" value="{{($project->projectDetail->language)??old('language')}}">
                         <label>Server Type</label>
-                        <input type="text" name="server_type" value="{{($project->server_type)??old('server_type')}}">
+                        <input type="text" name="server_type" value="{{($project->projectDetail->server_type)??old('server_type')}}">
                         <label>Hosted On</label>
-                        <input type="text" name="hosted_on" value="{{($project->hosted_on)??old('hosted_on')}}">
+                        <input type="text" name="hosted_on" value="{{($project->projectDetail->hosted_on)??old('hosted_on')}}">
                         <label>DNSSEC</label>
-                        <input type="text" name="dnssec" value="{{($project->dnssec)??old('dnssec')}}">
+                        <input type="text" name="dnssec" value="{{($project->projectDetail->dnssec)??old('dnssec')}}">
                         <label>Contact Email</label>
-                        <input type="text" name="contact_email" value="{{($project->contact_email)??old('contact_email')}}">
+                        <input type="text" name="contact_email" value="{{($project->projectDetail->contact_email)??old('contact_email')}}">
                         <label>Created Date</label>
-                        <input type="text" name="created_date" value="{{($project->created_date)??old('created_date')}}">
+                        <input type="text" name="created_date" value="{{($project->projectDetail->created_date)??old('created_date')}}">
                         <label>Expiry Date</label>
-                        <input type="text" name="expires_date" value="{{($project->expires_date)??old('expires_date')}}">
+                        <input type="text" name="expires_date" value="{{($project->projectDetail->expires_date)??old('expires_date')}}">
                         <label>Address</label>
-                        <input type="text" name="address" value="{{($project->address)??old('address')}}">
+                        <input type="text" name="address" value="{{($project->projectDetail->address)??old('address')}}">
                         <label>Alexa Rank</label>
-                        <input type="text" name="alexa_rank" value="{{($project->alexa_rank)??old('alexa_rank')}}">
+                        <input type="text" name="alexa_rank" value="{{($project->projectDetail->alexa_rank)??old('alexa_rank')}}">
                         <label>SSL</label>
-                        <input type="text" name="ssl" value="{{($project->ssl)??old('ssl')}}">
+                        <input type="text" name="ssl" value="{{($project->projectDetail->ssl)??old('ssl')}}">
                         <label>SSL Expiry</label>
-                        <input type="text" name="ssl_expiry" value="{{($project->ssl_expiry)??old('ssl_expiry')}}">
+                        <input type="text" name="ssl_expiry" value="{{($project->projectDetail->ssl_expiry)??old('ssl_expiry')}}">
                         <input type="submit">
                     </form>
                     </div>                   
