f448d08c91e02cd6c1c8350f4149175cfd5c738b
neemchand <neemchand@ucreate.co.in>
2018-07-13 17:54:31 +0530

    change password functionality

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,9 +15,50 @@
                     @endif
 
                     <div class="row">
-                        <div class="col-sm-12">
+                        <div class="col-md-12">
                             <div class="row">
-                               
+                                 <div class="col-md-12">
+                                <h4>Change password</h4>
+                                <form method="post" action="{{ url('/') }}" id="update_buyer_pwd_form" name="update_buyer_pwd_form">
+                                        {{ csrf_field() }}
+                                        <div class="col-md-12 ">
+                                        <div class="form-group col-md-6">
+                                            <div class="col-md-6">
+                                                 <label>Current password <span class="notification-star-buyer">*</span></label>
+                                            </div> 
+                                            <div class="col-md-6 ">
+                                                <input class="form-control" tabindex="35" maxlength="30" size="30" type="password" name="old_password" id="old_password">
+                                                <div class="error-message" style="display: none;" id="validation_error_existing_password"></div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <div class="col-md-6">
+                                             <label>New password <span class="notification-star-buyer">*</span></label>
+                                            </div> 
+                                            <div class="col-md-6 ">
+                                               <input class="form-control" tabindex="36" maxlength="30" size="30" type="password" name="new_password" id="new_password">
+                                               <div class="error-message" style="display: none;" id="validation_error_reset_password"></div>
+                                               
+                                            </div>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <div class="col-md-6">
+                                             <label>Confirm new password <span class="notification-star-buyer">*</span></label>
+                                            </div> 
+                                            <div class="col-md-6 ">
+                                              <input class="form-control" tabindex="37" maxlength="30" size="30" type="password" name="confirm_password" id="confirm_password">
+                                                 <div class="error-message" style="display: none;" id="validation_error_confirm_password"></div>
+                                              
+                                            </div>
+                                        </div>
+                                    
+                                        <div class="col-md-6">
+                                            <button  tabindex="38" type="button" class="btn btn-primary" id="buyer_update_pass_btn">Save</button>
+                                        </div>
+                                           </div>
+                                    </form>
+                                
+                                 </div>
                             </div><!--/row-->    
                         </div><!--/col-12-->
                     </div><!--/row-->
