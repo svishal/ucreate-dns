0a476222e6e181a2e742eada23a93862153fa2d5
neemchand <neemchand@ucreate.co.in>
2018-07-16 14:55:33 +0530

    search query updated

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -20,9 +20,12 @@ class Project extends Model
     }
     public static function searchProject($key, $date='', $filter_by_field = false){
         if(!empty($key)){
-            $query = ProjectDetail::where([['short_name', 'iLIKE', '%' .strtolower($key) . '%']])
-               ->orWhere([['name', 'iLIKE', '%' .strtolower($key) . '%']])
-                ->join('projects', 'projects.id', '=', 'project_details.project_id');
+            $query = self::where([['projects.short_name', 'iLIKE', '%' .strtolower($key) . '%']])
+               ->orWhere([['projects.name', 'iLIKE', '%' .strtolower($key) . '%']])
+               ->orWhere([['project_details.language', 'iLIKE', '%' .strtolower($key) . '%']])
+               ->orWhere([['project_details.domain_registrar', 'iLIKE', '%' .strtolower($key) . '%']])
+               ->leftJoin('project_details', 'project_details.project_id', '=', 'projects.id');
+            
         }
         if($filter_by_field){
             if($filter_by_field=='having_ssl'){
